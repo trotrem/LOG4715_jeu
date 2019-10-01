0bbd1ab5b6ad9a7fc65dcfa5e096f8f720b649c7
JC <jeanchristophe.buteau@gmail.com>
2019-10-01 18:34:24 -0400

    début de la trajectoire des flèches

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 10f;
+
+    float h;
+    float k;
+    float a;
+
+    float u = 0;
+    Vector3 startPos;
+
+    bool initialized = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (initialized)
+        {
+            u += Time.deltaTime * speed;
+            transform.position = new Vector3(0f, a * Mathf.Pow(u - h, 2) + k + startPos.y, u + startPos.z);
+        }
+	}
+
+    public void Shoot(Vector3 startPos, Vector3 trajectoryTop)
+    {
+        this.startPos = startPos;
+        h = trajectoryTop.z - startPos.z;
+        k = trajectoryTop.y - startPos .y;
+        a = (- k) / Mathf.Pow(- h, 2);
+        initialized = true;
+    }
+}
---
+++ Assets/Scripts/BowController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowController : MonoBehaviour {
+
+    [SerializeField]
+    Transform ArrowSpawnPoint;
+
+    [SerializeField]
+    GameObject ArrowPrefab;
+
+    [SerializeField]
+    GameObject BowAxisCenter;
+
+    [SerializeField]
+    Transform ProjectileParent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootArrow();
+        }
+	}
+
+    void ShootArrow()
+    {
+        var arrow = Instantiate(ArrowPrefab, ArrowSpawnPoint.position, Quaternion.Euler(90, 0, 0), ProjectileParent);
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            var point = new Vector3(0f, hit.point.y, hit.point.z);
+            arrow.GetComponent<ArrowScript>().Shoot(ArrowSpawnPoint.position, point);
+            Debug.Log(point);
+        }
+        else
+        {
+            Debug.Log("t'as cliqué dans le vide t'es bad!");
+        }
+        
+    }
+}
