043b2aebee221564c89bc6fe727362d4c69ebf27
JC <jeanchristophe.buteau@gmail.com>
2019-10-22 18:02:17 -0400

    bfiabsi

---
+++ Assets/Scripts/Overlord.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overlord : MonoBehaviour {
+
+    [SerializeField]
+    float gravity = -30f;
+	// Use this for initialization
+	void Start () {
+        Physics.gravity = new Vector3(0, gravity, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
