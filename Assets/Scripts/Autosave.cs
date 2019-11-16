a8ab654756cb8f9f6f2f0dbda8056de987271190
Sebastien Chagnon <chagnon.s21@gmail.com>
2019-11-16 15:42:39 -0500

    Added autosave

---
+++ Assets/Scripts/Autosave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autosave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            c.gameObject.GetComponent<PlayerControler>().Autosave(new Vector3(0, gameObject.transform.position.y, gameObject.transform.position.z));
+        }
+    }
+}
